1513e47d35b6f460afa6b80bb42c0999d1701868
Kiran Kumar <codingkiran92@gmail.com>
2022-09-06 08:22:58 +0530

    deleted some unwanted code

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-// const realName = document.getElementById("name1");
-// const p = document.getElementById("para");
-
-// realName.addEventListener("input", () => {
-//   const str = realName.value;
-//   p.innerText = str.toUpperCase();
-// });
-
 const btn = document.getElementById("btn-one");
 const btn2 = document.getElementById("btn-two");
 const btn3 = document.getElementById("btn-three");
 
 const beaches = document.querySelectorAll("[data-set1]");
 const mountains = document.querySelectorAll("[data-set0]");
-
 
 btn.addEventListener("click", () => {
   for (let i = 0; i < beaches.length; i++) {
@@ -33,7 +24,6 @@ btn2.addEventListener("click", () => {
     beaches[i].style.display = "inline-block";
   }
 });
-
 
 btn3.addEventListener("click", () => {
   for (let i = 0; i < beaches.length; i++) {
